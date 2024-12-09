1306b132a49ad5e5dbdfce4478770536fd8e3bdf
anupam575 <tripathianupam208@gmail.com>
2024-12-09 23:23:22 +0530

    Add files via upload

---
+++ SumOfDigits.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Welcome to Factorial");
+    System.out.print("Please enter your number");
+    int num = input.nextInt();
+    int sum = sumOfDigits(num);
+    System.out.println("Sum of Digits is: " + sum);
+  }
+  public static int sumOfDigits(int num) {
+    int sum = 0;
+    while (num > 0) {
+      sum += num % 10;
+      num /= 10;
+      
+    }
+    return sum;
+  }
+}
